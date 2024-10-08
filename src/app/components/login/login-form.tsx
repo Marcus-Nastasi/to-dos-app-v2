749ac7d856f7923ed84f4615610566bf37a46851
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-10-08 01:44:24 -0300

    login page: creating login page

---
+++ src/app/components/login/login-form.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import { Box, Button, FormControl, FormHelperText, Typography } from "@mui/joy";
+import { Link, TextField } from "@mui/material";
+import { useState } from "react";
+
+export default function LoginForm() {
+   const [ email, setEmail ] = useState<string>();
+   const [ password, setPassword ] = useState<string>();
+
+   return (
+      <Box
+         marginTop={10}
+         padding={5}
+         height={'fit-content'}
+         bgcolor={'white'}
+         borderRadius={10}
+         sx={{
+            width: {
+               xs: '95%',
+               sm: '75%',
+               md: '65%',
+               lg: '50%',
+               xl: '30%'
+            }
+         }}
+      >
+         <Typography
+            marginBottom={6}
+            textAlign={'center'}
+            fontSize={40}
+            fontWeight={'semibold'}
+         >
+            To-dos app
+         </Typography>
+         <FormControl>
+            <TextField 
+               id="input_name" 
+               label="E-mail" 
+               variant='outlined' 
+               color='primary'
+               value={email}
+               onChange={(e) => setEmail(e.target.value)} 
+            />
+            <FormHelperText sx={{ marginBottom: 2 }}>enter your e-mail</FormHelperText>
+            <TextField 
+               id="input_password" 
+               label="Password" 
+               variant='outlined' 
+               color="primary" 
+               type="password"
+               value={password}
+               onChange={(e) => setPassword(e.target.value)} 
+            />
+            <FormHelperText sx={{ marginBottom: 2 }}>enter your password</FormHelperText>
+            <Button 
+               sx={{ 
+                  width: { sm: "80%", lg: '30%' }, 
+                  alignSelf: 'center'
+               }} 
+               color='primary'
+               variant='solid'
+            >
+               ENTER
+            </Button>
+            <FormHelperText sx={{ marginTop: 5, alignSelf: 'end' }}>
+               if you don't have an account, 
+               <Link href="/register" underline="always" color="primary">
+                  register
+               </Link>
+            </FormHelperText>
+         </FormControl>
+      </Box>
+   );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import LoginForm from "../components/login/login-form";
+import { Box } from "@mui/joy";
+
+export default function Login() {
+   return (
+      <Box
+         margin={0}
+         padding={0}
+         width={'100%'}
+         height={'100vh'}
+         bgcolor={'#2D2D2D'}
+         display={'flex'}
+         justifyContent={'center'}
+      >
+         <LoginForm />
+      </Box>
+   );
+}
